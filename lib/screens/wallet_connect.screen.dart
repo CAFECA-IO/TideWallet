1d0eaf6462ff3ccda2afcc13ddb107a571dbc7f3
Paul Huang <paul.huang@tideisun.com>
2021-02-20 15:06:06 +0800

    feat: update wallet connect screen

--- lib/screens/wallet_connect.screen.dart
+++ lib/screens/wallet_connect.screen.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/appBar.dart';
+import '../widgets/qrcode_view.dart';
 
 class WalletConnectScreen extends StatelessWidget {
   static const routeName = '/wallet-connect';
 
+  _scanResult() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: GeneralAppbar(routeName: routeName,),
-      body: Container(
-        child: Text('Hello WalletConnect Screen'),
+      extendBodyBehindAppBar: true,
+      appBar: GeneralAppbar(
+        routeName: routeName,
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          QRCodeView(
+            scanCallback: this._scanResult,
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/qrcode_view.dart
+++ lib/widgets/qrcode_view.dart
@@ -40,10 +40,9 @@ class _QRCodeViewState extends State<QRCodeView> {
   }
 
   Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
     var scanArea = (MediaQuery.of(context).size.width < 400 ||
             MediaQuery.of(context).size.height < 400)
-        ? 150.0
+        ? 200.0
         : 300.0;
     // To ensure the Scanner view is properly sizes after rotation
     // we need to listen for Flutter SizeChanged notification and update controller
