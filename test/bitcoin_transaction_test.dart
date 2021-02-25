0ed11d2696635c8c100803d41581c6aba27eaf7a
TzuHanLiang <crocodilestear@gmail.com>
2021-02-25 17:41:22 +0800

    feat: update btc tx test

--- test/bitcoin_transaction_test.dart
+++ test/bitcoin_transaction_test.dart
@@ -8,6 +8,7 @@ import 'package:tidewallet3/repositories/transaction_repository.dart';
 import 'package:tidewallet3/constants/account_config.dart';
 import 'package:tidewallet3/database/entity/utxo.dart';
 import 'package:tidewallet3/database/entity/account.dart';
+import 'package:tidewallet3/database/entity/currency.dart';
 import 'package:tidewallet3/database/entity/account_currency.dart';
 import 'package:tidewallet3/database/db_operator.dart';
 import 'package:tidewallet3/models/account.model.dart';
@@ -21,57 +22,6 @@ void main() {
 
   setUpAll(() async {
     await DBOperator().init(inMemory: true);
-    const accounts = [
-      {
-        "account_id": "8e951597-e720-424b-83ec-be57c2451a99",
-        "blockchain_id": "80000001",
-        "network_id": 0,
-        "currency_id": "8e1ea17f-38f5-42ab-a24b-82bf8abc851b",
-        "balance": "0.01033221",
-        "publish": false,
-        "account_index": "0"
-      }
-    ];
-    List<AccountEntity> _accounts = accounts
-        .map(
-          (acc) => AccountEntity(
-            accountId: acc['account_id'],
-            userId: 'userId',
-          ),
-        )
-        .toList();
-    await opt.accountDao.insertAccounts(_accounts);
-    AccountCore().setBitcoinAccountService();
-    List currencies = [
-      {
-        "blockchain_id": "80000001",
-        "currency_id": "8e1ea17f-38f5-42ab-a24b-82bf8abc851b",
-        "account_id": "8e951597-e720-424b-83ec-be57c2451a99",
-        "purpose": 44,
-        "account_index": "0",
-        "curve_type": 0,
-        "balance": "0",
-        "symbol": "BTC",
-        "icon":
-            "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/32/icon/btc.png",
-        "tokens": []
-      }
-    ];
-    final _currencies = currencies
-        .map(
-          (c) => AccountCurrencyEntity(
-              accountcurrencyId:
-                  c['account_id'] ?? c['account_token_id'], // TODO: Change name
-              accountId: c['account_id'],
-              numberOfUsedExternalKey: c['number_of_external_key'],
-              numberOfUsedInternalKey: c['number_of_internal_key'],
-              balance: c['balance'],
-              currencyId: c['currency_id'] ?? c['token_id'],
-              lastSyncTime: DateTime.now().millisecondsSinceEpoch),
-        )
-        .toList();
-
-    await DBOperator().accountCurrencyDao.insertCurrencies(_currencies);
   });
 
   tearDownAll(() async {
@@ -84,9 +34,79 @@ void main() {
       expect(utxos, []);
     });
     test('insertUtxo test', () async {
+      const accounts = [
+        {
+          "account_id": "8e951597-e720-424b-83ec-be57c2451a99",
+          "blockchain_id": "80000001",
+          "network_id": 0,
+          "currency_id": "8e1ea17f-38f5-42ab-a24b-82bf8abc851b",
+          "balance": "0.01033221",
+          "publish": false,
+          "account_index": "0"
+        }
+      ];
+      List<AccountEntity> _accounts = accounts
+          .map(
+            (acc) => AccountEntity(
+              accountId: acc['account_id'],
+              userId: 'userId',
+            ),
+          )
+          .toList();
+      await opt.accountDao.insertAccounts(_accounts);
+      const currencies = [
+        {
+          "currency_id": "8e1ea17f-38f5-42ab-a24b-82bf8abc851b",
+          "name": "Bitcoin Testnet",
+          "symbol": "BTC",
+          "type": 1,
+          "publish": false,
+          "decimals": 8,
+          "exchange_rate": null,
+          "icon":
+              "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/32/icon/btc.png"
+        }
+      ];
+      List<CurrencyEntity> _currencies = currencies
+          .map(
+            (c) => CurrencyEntity.fromJson(c),
+          )
+          .toList();
+      await opt.currencyDao.insertCurrencies(_currencies);
+      AccountCore().setBitcoinAccountService();
+      List<Map> accountsDetails = [
+        {
+          "blockchain_id": "80000001",
+          "currency_id": "8e1ea17f-38f5-42ab-a24b-82bf8abc851b",
+          "account_id": "8e951597-e720-424b-83ec-be57c2451a99",
+          "purpose": 44,
+          "account_index": "0",
+          "curve_type": 0,
+          "balance": "0",
+          "symbol": "BTC",
+          "icon":
+              "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/32/icon/btc.png",
+          "tokens": []
+        }
+      ];
+      List<AccountCurrencyEntity> _accountsDetails = accountsDetails
+          .map(
+            (c) => AccountCurrencyEntity(
+                accountcurrencyId: c['account_id'] ??
+                    c['account_token_id'], // TODO: Change name
+                accountId: c['account_id'],
+                numberOfUsedExternalKey: c['number_of_external_key'],
+                numberOfUsedInternalKey: c['number_of_internal_key'],
+                balance: c['balance'],
+                currencyId: c['currency_id'] ?? c['token_id'],
+                lastSyncTime: DateTime.now().millisecondsSinceEpoch),
+          )
+          .toList();
+
+      await DBOperator().accountCurrencyDao.insertCurrencies(_accountsDetails);
       List<JoinUtxo> utxos =
           await opt.utxoDao.findAllJoinedUtxosById('currencyId');
-      Log.warning('utxos: ${hex.encode(Uint8List(0))}');
+      Log.warning('hex.encode(Uint8List(0)): ${hex.encode(Uint8List(0))}');
 
       if (utxos.isEmpty) {
         UtxoEntity _utxo = UtxoEntity.fromUnspentUtxo(UnspentTxOut(
@@ -106,8 +126,8 @@ void main() {
         await opt.utxoDao.insertUtxo(_utxo);
       }
       utxos = await opt.utxoDao.findAllJoinedUtxosById('currencyId');
+      Log.warning('utxos: $utxos');
       expect(utxos.isNotEmpty, true);
-      Log.debug('utxos: $utxos');
     });
     test('Bitcoin prepareTransaction test', () async {
       TransactionRepository _repo = TransactionRepository();
