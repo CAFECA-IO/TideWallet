47974a80954b2fcab4e03e4655a392c5102dd3a8
TzuHanLiang <crocodilestear@gmail.com>
2021-02-26 18:08:24 +0800

    feat: remove useless log

--- lib/screens/transaction_detail.screen.dart
+++ lib/screens/transaction_detail.screen.dart
@@ -239,7 +239,6 @@ class _TransactionDetailScreenState extends State<TransactionDetailScreen> {
 
 _launchURL(String url) async {
   if (await canLaunch(url)) {
-    print(url);
     await launch(url);
   } else {
     throw 'Could not launch $url';
