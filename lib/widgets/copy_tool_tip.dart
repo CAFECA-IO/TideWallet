f15e34af34aff6025f4f3169db747592fc2459db
TzuHanLiang <crocodilestear@gmail.com>
2021-03-12 13:58:56 +0800

    feat: add some code

--- lib/widgets/copy_tool_tip.dart
+++ lib/widgets/copy_tool_tip.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../helpers/i18n.dart';
+
+final t = I18n.t;
+
 class CopyToolTip extends StatelessWidget {
   final Widget child;
   final String text;
@@ -10,7 +14,7 @@ class CopyToolTip extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Tooltip(preferBelow: false, message: "Copy", child: child),
+      child: Tooltip(preferBelow: false, message: t('copy'), child: child),
       onTap: () {
         Clipboard.setData(ClipboardData(text: text));
       },
