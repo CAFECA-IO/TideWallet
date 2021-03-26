ec9b9da1f91d7d02aff6c4f3c4e2012f8e2c03f4
Paul Huang <paul.huang@tideisun.com>
2021-03-26 10:36:42 +0800

    fix: utxo entity FK constraint

--- lib/database/entity/utxo.dart
+++ lib/database/entity/utxo.dart
@@ -12,6 +12,7 @@ import '../../models/bitcoin_transaction.model.dart';
       childColumns: ['accountcurrency_id'],
       parentColumns: ['accountcurrency_id'],
       entity: AccountCurrencyEntity,
+      onDelete: ForeignKeyAction.cascade,
     )
   ],
 )
