2dab6f5d68a41686acb89be016bf0519bf33ba3b
Paul Huang <zedozon@gmail.com>
2021-05-19 16:10:04 +0800

    feat: explorer link
    set link by different ACCOUNT
    
    issue #113

--- lib/constants/account_config.dart
+++ lib/constants/account_config.dart
@@ -1,3 +1,9 @@
 enum ACCOUNT { BTC, ETH, CFC, XRP }
 enum NETWORK { MAINNET, TESTNET }
 const NETWORK USE_NETWORK = NETWORK.MAINNET;
+
+class Explorer {
+  static const ETHERSCAN = 'etherscan.io';
+  static const TITAN_EXPLORER = 'explorer.cafeca.io';
+  static const BLOCK_EXPLORER = 'blockexplorer.one';
+}
